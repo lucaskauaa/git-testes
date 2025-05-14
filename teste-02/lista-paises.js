3eb0041d4d9889cae228f4f15827cab74dffab14
lucaskauaa <lk051015lk@gmail.com>
2025-05-14 15:00:58 -0300

    Finalizada função de exibir países

--- teste-02/lista-paises.js
+++ teste-02/lista-paises.js
@@ -1,1 +1,9 @@
 const paises = ["Brasil", "Canadá", "Rússia", "China", "Austrália", "Angola"];
+
+function exibirPaises (paises) {
+    for (let i = 0; i < paises.length; i++) {
+        console.log(paises[i]);
+    }
+}
+
+exibirPaises(paises);
